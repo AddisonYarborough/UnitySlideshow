f3ea8cd8f494a68337ab50c14c722819cfb3ec75
Addyarb <Addyarb@gmail.com>
2020-08-17 11:51:39 -0400

    SlideManager stability improvements
    - Configure Key Codes for next/previous slides in inspector
    - Configure fade duration from inspector
    - Cleaned up logic to handle error states when moving to previous / next slide too quickly

--- SlideManager.cs
+++ SlideManager.cs
@@ -1,77 +1,138 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+
+public class SlideManager : MonoBehaviour {
+    //All _slides in the project, minus the _fadeSlide
+    [SerializeField]
+    private List<Transform> _slides = default;
+
+    //The panel that overlays all _slides and changes from clear to black
+    [SerializeField]
+    private Image _fadeSlide;
+
+    [Header("Config Values")]
+    [SerializeField, Tooltip("The duration (in seconds) over which the fade slide will fade in / out")]
+    private float _fadeDuration = 0.75f;
+
+    [SerializeField, Tooltip("All key codes that will move to the next slide if pressed")]
+    private KeyCode[] _nextSlideKeyCodes = {
+        KeyCode.D,
+        KeyCode.RightArrow,
+        KeyCode.Space
+    };
+
+    [SerializeField, Tooltip("All key codes that will move to the previous slide if pressed")]
+    private KeyCode[] _previousSlideKeyCodes = {
+        KeyCode.A,
+        KeyCode.LeftArrow,
+    };
+
+    //The slide we're currently viewing
+    private int _currentSlide;
+
+    // Whether the fade slide is currently fading
+    private bool _isTransitioning = false;
+
+    private void Start() {
+        //We set the _currentSlide to -1 so that when it's incremented, it will be the first (0th) slide
+        _currentSlide = -1;
+
+        // Set our fade to black slide to black so that the audience can not see the first slide
+        _fadeSlide.color = Color.black;
+    }
+
+    public void Update() {
+        //Check for arrow keys/space bar down so we can fade the fadePanel accordingly
+        ListenForInput();
+    }
+
+    private void ListenForInput() {
+        // Ignore input if we're in the middle of a transition
+        if (_isTransitioning) {
+            return;
+        }
+
+        //If we strike the space bar or right arrow key, proceed to the next slide
+        if (_nextSlideKeyCodes.Any(Input.GetKeyDown)) {
+            NextSlide();
+        }
+
+        //If we strike the left arrow key, return to the previous slide
+        if (_previousSlideKeyCodes.Any(Input.GetKeyDown)) {
+            PreviousSlide();
+        }
+
+        //If we strike the Esc key, quit the application
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Application.Quit();
+        }
+    }
+
+    private void NextSlide() {
+        //If we're on the very last slide already..
+        if (_currentSlide == _slides.Count - 1) {
+            // Exit early
+            return;
+        }
+
+        //Increment the slide count
+        _currentSlide++;
+
+        // Transition to the next slide
+        StartCoroutine(SlideTransition());
+    }
 
 
-public class SlideManager : MonoBehaviour
-	{
-	//All slides in the project, minus the fadeSlide
-	public List<Transform> slides;
-	//The panel that overlays all slides and changes from clear to black
-	public Image fadeSlide;
-	//Bools to control the fadePanel's color
-	public bool fadeToBlack, fadeToClear;
-	//The slide we're currently viewing
-	public int currentSlide;
+    private IEnumerator SlideTransition() {
+        // Mark our fading slide as currently fading
+        _isTransitioning = true;
 
-	void Start()
-		{
-		currentSlide = -1;//We set the currentSlide to -1 so that when it's incremented, it will be the first (0th) slide
-		NextSlide(); //Proceed to the first slide
-		}
+        // Fade to black
+        yield return StartCoroutine(FadeToTargetColor(targetColor: Color.black));
 
-	public void Update()
-		{
-		FadeSlides(); //Apply the color lerping to the fade panel, which overlays all slides
-		ListenForInput(); //Check for arrow keys/space bar down so we can fade the fadePanel accordingly
-		}
+        // Set only our current slide active - and all others inactive
+        _slides.ForEach(slide => slide.gameObject.SetActive(_slides.IndexOf(slide) == _currentSlide));
 
-	void FadeSlides()
-		{
-		//Fade the fadePanel by lerping the color from it's current color to clear or black, depending on the bool
-		if (fadeToBlack) fadeSlide.color += new Color(0, 0, 0, 1 * Time.deltaTime / 2);
-		if (fadeToClear) fadeSlide.color -= new Color(0, 0, 0, 1 * Time.deltaTime / 2);
-		if (fadeToClear && fadeSlide.color.a <= 0) fadeToClear = false; //Disable fadeToClear if we are already at minimum alpha (to avoid unnecessary updates)
-		}
+        // Fade to clear
+        yield return StartCoroutine(FadeToTargetColor(targetColor: Color.clear));
 
-	void ListenForInput()
-		{
-		if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.RightArrow)) NextSlide(); //If we strike the spacebar or right arrow key, proceed to the next slide
-		if (Input.GetKeyDown(KeyCode.LeftArrow)) PreviousSlide(); //If we strike the left arrow key, return to the previous slide
-		if (Input.GetKeyDown(KeyCode.Escape)) Application.Quit(); //If we strike the Esc key, quit the application
-		}
+        // Mark our fading slide as no longer fading
+        _isTransitioning = false;
+    }
 
-	public void NextSlide()
-		{
-		if (currentSlide == slides.Count - 1) return; //If we're on the very last slide, ignore the rest of the method
-		currentSlide++; //Increment the slide count
-		StartCoroutine(SlideTransition(true)); //Start the slide transition (fade to black, then to clear)
-		}
+    private void PreviousSlide() {
+        //If the current slide is the very first slide, ignore the rest of this method
+        if (_currentSlide == 0) {
+            return;
+        }
 
+        //Decrement the current slide
+        _currentSlide--;
 
-	IEnumerator SlideTransition(bool isNext) //If isNext is true, then we go to next slide, else we go to the previous slide
-		{
-		fadeToBlack = true; //Lerp the overlay panel to black
-		yield return new WaitForSeconds(2); //Wait for lerping to finish
-		//--Check if enabling previous or next slide..
-		if (isNext) //If we're going to the next slide..
-			{ //If we're not on the first slide, disable the previous slide
-			if (currentSlide > 0) slides[currentSlide - 1].gameObject.SetActive(false);
-			} else
-			{ //If we're going to the previous slide.. and if the current slide is not the last slide - disable the following slide
-			if (currentSlide < slides.Count - 1) slides[currentSlide + 1].gameObject.SetActive(false);
-			}
-		slides[currentSlide].gameObject.SetActive(true); //Enable the currentSlide
-		fadeToBlack = false; //Disable the fadeToBlack bool since we're done fading to black
-		fadeToClear = true; //Now that we've done our disabling/enabling, fade to clear
-		yield return new WaitForSeconds(2); //Wait for the lerping to finish
-		}
-	
-	void PreviousSlide()
-		{
-		if (currentSlide == 0) return; //If the current slide is the first slide, ignore the rest of this method
-		currentSlide--; //Decrement the current slide
-		StartCoroutine(SlideTransition(false)); //Start the slide transition (fade to black, then to clear)
-		}
-	}
+        // Transition to the previous slide
+        StartCoroutine(SlideTransition());
+    }
+
+    private IEnumerator FadeToTargetColor(Color targetColor) {
+        // The total amount of seconds that has elapsed since the start of our lerp sequence
+        float elapsedTime = 0.0f;
+
+        // The color of our fade panel at the start of the lerp sequence
+        Color startColor = _fadeSlide.color;
+
+        // While we haven't reached the end of the lerp sequence..
+        while (elapsedTime < _fadeDuration) {
+            // Increase our elapsed time
+            elapsedTime += Time.deltaTime;
+
+            // Perform a lerp to our target color
+            _fadeSlide.color = Color.Lerp(startColor, targetColor, elapsedTime / _fadeDuration);
+
+            // Wait for the next frame
+            yield return null;
+        }
+    }
+}
